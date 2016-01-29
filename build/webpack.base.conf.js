9358beb9eac8284c731634b05e69e70f620b3568
Marc-Antoine Bouchard Marceau <marc-antoine.bm@libeo.com>
2016-01-28 19:37:13 -0500

    Require script and css with loader!!

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -40,7 +40,11 @@ module.exports = {
           limit: 10000,
           name: '[name].[ext]?[hash]'
         }
-      }
+      },
+      {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff2" },
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" }
     ]
   },
   vue: {
--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,20 @@
         </div>
         <div class="col-lg-4">
             <h1>Rules</h1>
-            <img src="https://s-media-cache-ak0.pinimg.com/originals/92/a6/9e/92a69ea14fb0600a99a7322edc280ba8.gif" alt="">
+            <img src="https://s-media-cache-ak0.pinimg.com/originals/92/a6/9e/92a69ea14fb0600a99a7322edc280ba8.gif" alt="" class="img-responsive">
         </div>
     </div>
 </template>
 
 <script>
+    require("style!css!bootstrap/dist/css/bootstrap.css");
+    require("style!css!bootstrap-material-design/dist/css/bootstrap-material-design.css");
+    require("style!css!bootstrap-material-design/dist/css/ripples.css");
+
+    require("script!jquery/dist/jquery.min.js");
+    require("script!bootstrap-material-design/dist/js/material.min.js");
+    require("script!bootstrap-material-design/dist/js/ripples.min.js");
+
     import PokerTimer from './components/Poker-timer.vue'
     import PokerWinner from './components/Poker-winner.vue'
     import PokerTokens from './components/Poker-tokens.vue'
@@ -34,6 +42,10 @@
             }
         }
     }
+
+    $( document ).ready(function() {
+        $.material.init();
+    });
 </script>
 
 <style>
